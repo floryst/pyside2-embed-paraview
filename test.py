f9037be1bdda48295adcec972cc887ce6cee2852
Forrest Li <forrest.li@kitware.com>
2017-07-27 13:10:05 -0400

    Generate UI in python and reparent CPP window widget

--- test.py
+++ test.py
@@ -1,10 +1,30 @@
 import sys
 sys.path.append('.')
-from PySide2.QtWidgets import QApplication
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
 import foo
 
-app = QApplication(sys.argv)
-window = foo.Goba.MyWindow()
+class PyWindow(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
 
-window.show()
+        self.setWindowTitle("PyWindow")
+
+        self.layout = QVBoxLayout()
+
+        label = QLabel("hello")
+        self.layout.addWidget(label)
+
+        self.fooWindow = foo.Goba.MyWindow()
+        # 0 == Qt::Widget
+        self.fooWindow.setWindowFlags(0)
+        self.layout.addWidget(self.fooWindow)
+
+        self.setLayout(self.layout)
+
+app = QApplication(sys.argv)
+
+pyWindow = PyWindow()
+pyWindow.show()
+
 app.exec_()
