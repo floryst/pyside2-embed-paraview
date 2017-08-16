2ad6583cc06dd639d23d9991110a0a25e538ffc6
Forrest Li <forrest.li@kitware.com>
2017-08-16 13:49:10 -0400

    Refactor: Rename libfoo to libpvWidget and foo to pypvBinding
    These names are more clear than the nondescript "foo" and "libfoo"
    identifiers.

---
+++ libpvWidget/pvwidget.h
@@ -0,0 +1,26 @@
+#ifndef _PVWIDGET_H
+#define _PVWIDGET_H
+
+#include <QtCore>
+#include <QMainWindow>
+
+namespace Ui {
+class PVWidget;
+}
+
+class pqPVApplicationCore;
+
+class PVWidget : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+    explicit PVWidget(QWidget* parent = 0);
+    ~PVWidget();
+
+private:
+    Ui::PVWidget *ui;
+
+    pqPVApplicationCore* pvAppCore;
+};
+#endif
